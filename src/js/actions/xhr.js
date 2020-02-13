503f9bfc734980b127577cdfe06f774dd72eae05
taeuk-gang <taeuk_kang@naver.com>
2020-02-13 14:35:00 +0900

    :new: #12 XHR 공통 함수 구현
    - close #12

---
+++ src/js/actions/xhr.js
@@ -0,0 +1,21 @@
+export const loadXhr = obj => new Promise((resolve, reject) => {
+	const req = new XMLHttpRequest()
+
+	if (!req) {
+		throw new Error(`No exist XHR`)
+	}
+
+	req.open(obj.method, obj.url)	
+
+	req.setRequestHeader(`x-requested-with`, `XMLHttpRequest`)
+	req.onreadystatechange = () => {
+		if (req.readyState === XMLHttpRequest.DONE) {
+			if (req.status === 200 || req.status === 201) {
+				resolve(req.responseText)			
+			} else {
+				reject(req.statusText)
+			}
+		}
+	}
+	req.send(obj.body || null)
+})
--- src/js/content.js
+++ src/js/content.js
@@ -1,7 +1,21 @@
 // import './components/test-element.js'
 import './components/css-api.js'
 import './components/ocr-api.js'
+import { loadXhr } from './actions/xhr.js'
 
 // TEST
 // document.body.appendChild(document.createElement(`test-element`))
+
+async function xhrTest() {
+	const testXhrData = await loadXhr({
+		method: `GET`,
+		url: `https://www.naver.com`,
+	})
+  
+	console.info(testXhrData)
+}
+
+// 가빈형 이거 쓰세염
+xhrTest()
+
 console.info(`content-script start!`)
