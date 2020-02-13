e5409e59251175d846f77e0f7c86af81b41e1e65
taeuk-gang <taeuk_kang@naver.com>
2020-02-14 01:46:05 +0900

    hotfix

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,14 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)\/(?!(lit-html))/,
+				test: /\.js$/,
 				use: {
 					loader: `babel-loader`,
 					options: {
-						presets: [`@babel/preset-env`],
+						presets: [
+							[`@babel/preset-env`, {targets: {ie: `11`}}],
+						],
+						plugins: [`@babel/plugin-syntax-dynamic-import`],
 					},
 				},
 			},
