733a63c95520b78af524dbb0d751f6598326782f
myh9410 <mjskks94@naver.com>
2020-02-13 23:56:08 +0900

    :new: #15 OCR API 활용해서 텍스트 추출, 배열에 저장 후 반환

--- src/js/actions/xhr.js
+++ src/js/actions/xhr.js
@@ -9,13 +9,10 @@ export const loadXhr = obj => new Promise((resolve, reject) => {
 
 	req.open(obj.method, obj.url)	
 	req.setRequestHeader(`x-requested-with`, `XMLHttpRequest`)
-	if(typeof obj.header != `undefined`){
-		obj.header.forEach(each => {
-			req.setRequestHeader(each.key, each.value)
-		})
-	
-	}
-	
+	obj.header.forEach(each => {
+		req.setRequestHeader(each.key, each.value)
+	})
+
 	req.onreadystatechange = () => {
 		if (req.readyState === XMLHttpRequest.DONE) {
 			if (req.status === 200 || req.status === 201) {
